04b575c5fc34949cba024bce431e72dc5e755a0d
Luis G. Villegas <luisguve@gmail.com>
2021-07-09 16:10:50 -0300

    npx create-strapi-app tutoru-backend --quickstart

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '579ef804183718f4f1e508aaf693c6fd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4c9821c0-0668-4e61-b093-1dc3e5c02c44'
+};
